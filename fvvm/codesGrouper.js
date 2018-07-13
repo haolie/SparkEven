075a6116be08d737e55c2fe189cc946deb7bf9b5
LiuYouhao <haolie2006@163.com>
2018-07-13 18:36:14 +0800

    修改获取编号方式

---
+++ fvvm/codesGrouper.js
@@ -0,0 +1,57 @@
+var http = require('http');
+var async = require("async");
+var nohelper = require('./nohelper.js');
+var url = require("url");
+var path = require('path');
+var fs = require('fs');
+var tool = require('./tools.js');
+
+
+var grouper = function () {
+
+}
+
+var group = function () {
+    var date = "2018-7-13", path = "D:\\d\\", index = 0;
+    fs.readFile(path + "old.txt", function (err, bytesRead) {
+
+        var oldcodes = bytesRead.toString().split("\r");
+        var oldObj = {}
+        oldcodes.forEach(function (c) {
+            c=c.replace(/\n/,"");
+            c=c.replace(/\n/,"");
+            c=c.replace(/ /,"");
+            oldcodes[c] = 1;
+        })
+
+        nohelper.getallnofromweb("", function (err, codes) {
+            var list = [];
+            codes.forEach(function (c) {
+
+                if (!oldcodes[c.no]) list.push(c)
+            })
+
+            list = tool.getSpiedList(list, 500);
+
+
+            async.mapLimit(list, 1, function (group, callback) {
+                var str = "";
+                group.forEach(function (g) {
+                    str += g.no + "\r\n "
+                })
+                index += 1;
+                var tmp = path + date + "_" + index + ".txt";
+                fs.writeFile(tmp, str, function (err) {
+                    console.log(tmp);
+                    callback(err, 1)
+                });
+
+            }, function (err, result) {
+                console.log("finished")
+            })
+        })
+
+    });
+}
+
+group();
